c2a8b0e92761082169ad76f25e9a23223ec9e1b7
Fahris12344 <farismhd255@gmail.com>
2024-10-18 15:06:33 +0700

    .

--- resources/views/topbar.blade.php
+++ resources/views/topbar.blade.php
@@ -10,11 +10,11 @@
                 </div><!-- end left -->
                 <div class="col-md-6 col-sm-6 hidden-xs text-right">
                     <div class="social">
-                        <a class="facebook" href="#" data-tooltip="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>              
-                        <a class="twitter" href="#" data-tooltip="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
-                        <a class="google" href="#" data-tooltip="tooltip" data-placement="bottom" title="Google Plus"><i class="fa fa-google-plus"></i></a>
-                        <a class="linkedin" href="#" data-tooltip="tooltip" data-placement="bottom" title="Linkedin"><i class="fa fa-linkedin"></i></a>
-                        <a class="pinterest" href="#" data-tooltip="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                        <a class="facebook" href="https://www.facebook.com/gaming/play?store_visit_source=games_bookmark" data-tooltip="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>              
+                        <a class="twitter" href="https://x.com/bni" data-tooltip="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                        <a class="google" href="https://edu.google.com/intl/ALL_id/communications/?utm_source=8a_googleEDU&utm_medium=cpc&utm_campaign=global-EDU-operational-cs-edu-fa-gp-cpm-paid-media-audience-expansion&utm_content=21145112370&utm_term=google%20for%20education&gad_source=1&gclid=CjwKCAjwjsi4BhB5EiwAFAL0YHdbhtDBy44wsKgywnjBqI88ZS0p8Fvona2wykGOgOteo0cptQeBHhoC3NQQAvD_BwE&gclsrc=aw.ds" data-tooltip="tooltip" data-placement="bottom" title="Google Plus"><i class="fa fa-google-plus"></i></a>
+                        <a class="linkedin" href="https://www.linkedin.com/learning" data-tooltip="tooltip" data-placement="bottom" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+                        <a class="pinterest" href="https://id.pinterest.com/search/pins/?q=sekolah&rs=typed" data-tooltip="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
                     </div><!-- end social -->
                 </div><!-- end left -->
             </div><!-- end row -->
